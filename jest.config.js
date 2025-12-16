35f8adf02e808f66c7cf18b36cb28114502e0687
Dan-Håkan Davall <dd222mk@student.lnu.se>
2025-12-16 09:26:09 +0100

    Jest implementerat för cartController

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+export default {
+    testEnvironment: 'node',
+    transform: {},
+    injectGlobals: true,
+    testMatch: [
+        '**/__tests__/**/*.test.js',
+        '**/?(*.)+(spec|test).js'
+    ],
+    collectCoverageFrom: [
+        'controllers/**/*.js',
+        'models/**/*.js',
+        'routes/**/*.js',
+        '!**/__tests__/**'
+    ]
+}
